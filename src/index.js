a7c25dc9a113e3319e11eacffc0c5018e106c564
odaney baker <bakerobtb@gmail.com>
2023-04-16 21:04:50 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let form = document.querySelector('#create-task-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  buildTaskList(e.target.querySelector('#new-task-description').value);
+  form.requestFullscreen();
+ })
 });
